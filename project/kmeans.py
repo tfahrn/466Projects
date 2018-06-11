384dcc95331a7650a28ac6408d1a3c2e0aa1b0dd
Thomas Fahrner <33201730+tfahrn@users.noreply.github.com>
2018-06-11 01:32:14 -0700

    added kmeans clustering; results for elbow method and silhouette coeff

---
+++ project/kmeans.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
+
+
+def get_data():
+    data = np.loadtxt(open('data/encoded_data.csv', "rb"), delimiter=",")
+
+    return data
+
+
+def main():
+    data = get_data()
+    sse = {}
+
+    for k in range(2, 10):
+        kmeans = KMeans(n_clusters=k).fit(data)
+        labels = kmeans.labels_
+        sse[k] = kmeans.inertia_
+        sil_coeff = silhouette_score(data, labels, metric='euclidean')
+        print("Silhouette Coefficient with {} clusters: {}".format(k, sil_coeff))
+
+    """
+    plt.figure()
+    plt.plot(list(sse.keys()), list(sse.values()))
+    plt.title("k-means Clustering on Turkey Political Opinions")
+    plt.xlabel("Number of clusters")
+    plt.ylabel("SSE")
+    plt.show()
+    """
+
+
+    
+
+if __name__ == '__main__':
+    main()
