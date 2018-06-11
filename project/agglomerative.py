76f40d667eb6fcc3516c7cfacd4ed08f8d228b4b
Thomas Fahrner <33201730+tfahrn@users.noreply.github.com>
2018-06-11 02:15:40 -0700

    finished agglomerative hierarchical clustering

---
+++ project/agglomerative.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering 
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
+
+
+def get_data():
+    data = np.loadtxt(open('data/encoded_data.csv', "rb"), delimiter=",")
+
+    return data
+
+
+def main():
+    data = get_data()
+    sse = {}
+
+    for k in range(2, 10):
+        agg = AgglomerativeClustering(n_clusters=k, linkage='average').fit(data)
+        labels = agg.labels_
+        #sse[k] = agg.inertia_
+        sil_coeff = silhouette_score(data, labels, metric='euclidean')
+        print("Silhouette Coefficient with {} clusters: {}".format(k, sil_coeff))
+
+    """
+    plt.figure()
+    plt.plot(list(sse.keys()), list(sse.values()))
+    plt.title("k-means Clustering on Turkey Political Opinions")
+    plt.xlabel("Number of clusters")
+    plt.ylabel("SSE")
+    plt.show()
+    """
+
+
+    
+
+if __name__ == '__main__':
+    main()
